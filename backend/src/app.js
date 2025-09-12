8e4d191b186f43c668c2a263c1962967ed7b83e4
Oluwasegun Ige <mailshegze@gmail.com>
2025-09-12 22:38:52 +0100

    chore: setup cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
 const healthRoutes = require('./routes/health.routes');
 const authRoutes = require('./routes/auth.routes');
 
+app.use(cors({ origin: 'http://localhost:3001' }));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
