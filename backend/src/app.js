7fd07141fd62b3fb773ce85a9009db5ec429e85f
Oluwasegun Ige <mailshegze@gmail.com>
2025-09-17 22:24:54 +0100

    fix: get allowed origins from env

--- backend/src/app.js
+++ backend/src/app.js
@@ -24,7 +24,25 @@ const paymentRoutes = require('./routes/payment.routes');
 
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
-app.use(cors({ origin: 'http://localhost:3001', credentials: true }));
+const allowedOrigins = (process.env.ALLOWED_ORIGINS || '').split(',');
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 
