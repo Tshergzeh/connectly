d84f09dab6f5c4e0aa08d81222357e074b3feecb
Oluwasegun Ige <mailshegze@gmail.com>
2025-09-25 12:13:49 +0100

    chore: add focus accessbility to Button component

--- frontend/components/ui/Button.jsx
+++ frontend/components/ui/Button.jsx
@@ -1,7 +1,7 @@
 export default function Button({ children, className = '', ...props }) {
   return (
     <button
-      className={`px-3 py-1.5 sm:px-4 sm:py-2 text-sm sm:text-base bg-indigo-600 text-white hover:bg-indigo-800 transition disabled:opacity-50 disabled:cursor-not-allowed ${className}`}
+      className={`px-3 py-1.5 sm:px-4 sm:py-2 text-sm sm:text-base bg-indigo-600 text-white hover:bg-indigo-800 transition disabled:opacity-50 disabled:cursor-not-allowed focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:ring-offset-2 ${className}`}
       {...props}
     >
       {children}
