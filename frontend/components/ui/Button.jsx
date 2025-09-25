15dbe2bba42de6eb2e664e2c90b97856b6e20387
Oluwasegun Ige <mailshegze@gmail.com>
2025-09-25 12:10:28 +0100

    chore: change button style when disabled

--- frontend/components/ui/Button.jsx
+++ frontend/components/ui/Button.jsx
@@ -1,7 +1,7 @@
 export default function Button({ children, className = '', ...props }) {
   return (
     <button
-      className={`px-3 py-1.5 sm:px-4 sm:py-2 text-sm sm:text-base bg-indigo-600 text-white hover:bg-indigo-800 transition ${className}`}
+      className={`px-3 py-1.5 sm:px-4 sm:py-2 text-sm sm:text-base bg-indigo-600 text-white hover:bg-indigo-800 transition disabled:opacity-50 disabled:cursor-not-allowed ${className}`}
       {...props}
     >
       {children}
