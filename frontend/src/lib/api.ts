8c8dca82da3421e9d0e56f6e9fcfa9cffffc9aaf
Oluwasegun Ige <mailshegze@gmail.com>
2025-10-15 16:44:28 +0100

    feat: update loginUser such that cookies are set

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -3,6 +3,7 @@ export async function loginUser(email: string, password: string) {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ email, password }),
+    credentials: 'include',
   });
 
   let data;
