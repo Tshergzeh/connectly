2f34f5bf09182a87bb7fbd1c01b29422bc8d09de
Oluwasegun Ige <mailshegze@gmail.com>
2025-10-18 13:45:39 +0100

    wrap ServicesList in Suspense

--- frontend/app/services/page.tsx
+++ frontend/app/services/page.tsx
@@ -1,8 +1,23 @@
+'use client';
+
+import { Loader2 } from 'lucide-react';
+import { Suspense } from 'react';
+
 import { fetchServices } from '~/lib/api/services';
 import ServicesList from '~/components/services/ServicesList';
 
 export default async function ServicesPage() {
   const limit = 10;
   const initialData = await fetchServices(undefined, limit);
-  return <ServicesList initialData={initialData} initialLimit={limit} />;
+  return (
+    <Suspense
+      fallback={
+        <div className="flex justify-center py-20">
+          <Loader2 className="animate-spin w-6 h-6 text-gray-500" />
+        </div>
+      }
+    >
+      <ServicesList initialData={initialData} initialLimit={limit} />;
+    </Suspense>
+  );
 }
